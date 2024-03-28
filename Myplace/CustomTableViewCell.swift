51429b7833d2b2e85270e8b106a3fa4926577ae3
Yaraul1 <yaroslav.yuferev@gmail.com>
2024-03-28 15:10:17 +0300

    Add New Place Scene usinf NavigationController, add there image and 4 rows where user can write his place information

---
+++ Myplace/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  Myplace
+//
+//  Created by Yaroslav on 27.03.2024.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
